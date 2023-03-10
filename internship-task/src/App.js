1e84e1dcc0dc6b3548299a3095b353dd1f4dfa2a
vladimir antonovski <vladimir.antonovski@gmail.com>
2023-03-10 22:19:51 +0100

    onclick

--- internship-task/src/App.js
+++ internship-task/src/App.js
@@ -1,57 +1,54 @@
-import './App.css';
-import Books from './listofbooks.json';
-import { useState } from 'react'
+import "./App.css";
+import Books from "./listofbooks.json";
+import { useState } from "react";
+
+
+
+
+
 
 function App() {
 
-  const [inputText, setInputText] = useState("");
+  const [value, setValue] = useState('')
 
-  let inputHandler = (e) => {
+  const OnChange = (event) => {
+    setValue(event.target.value);
+  }
 
-    let lowercase = e.target.value.toLowerCase();
-    setInputText(lowercase)
+  const onSearch = (searchBook) => {
+    console.log("search", searchBook);
   }
 
 
+return (
 
 
+  
+    <div className="App">
+      <p className="heading">Search</p>
 
-  return (
-    <div className="App d-flex">
-      
-  <h1 className="heading">Enter a movie</h1>
-<div className="d-flex">
-  <div>
-  <input id="search_bar" onChange={inputHandler} className="search_bar" placeholder="enter a movie..." />
-  <label></label>
+<div className="search_container">
+
+  <div className="search_inner_container">
+    <input value={value} onChange={OnChange} type="text" placeholder="search for a book"/>
+    <button onClick={() => onSearch(value)}>SEARCH</button>
   </div>
-
-  <div>
-    <button className="button">Search a movie</button>
-  </div>
- 
-</div>
-
-<div className='books'>
-
-<div>
-  <p className="heading_two">Books</p>
-</div>
-
-<div className="books_section">
+<div className="books_container">
 {Books.map((data, post) =>
-<div className="all_books" key={post}>
+<div className="books" key={post}>
+
+  <div className="each_book">
   <p className="author">{data.author}</p>
   <p className="title">{data.title}</p>
-  <p>{data.genre}</p>
+  <p className="genre">{data.genre}</p>
+
+  </div>
 </div>
 )}
 </div>
-
-
 </div>
 
-      
+
     </div>
   );
 }
